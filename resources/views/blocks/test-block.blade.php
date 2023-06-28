301e6bee174b172b289ca2a972ff722e8c5f1601
Michael McKnight <michaelmcknight@mdawg-mbp.lan>
2023-06-27 20:37:39 -0500

    mmenu fix

--- resources/views/blocks/test-block.blade.php
+++ resources/views/blocks/test-block.blade.php
@@ -23,24 +23,10 @@
 @endphp
 
 <section data-{{ $block['id'] }} @if (($block['anchor'])) id="{{ $block['anchor'] }}" @endif class="{{ $block['classes'] }}">
-	<div class="container-lg background-color-primary full-width">
+	<div class="container-lg">
 		<div class="test-block__innerblocks">
 			<InnerBlocks template="{{ wp_json_encode( $template ) }}" />
 		</div>
-		<div class="flex gap-1 gap-md-3 cols-2 cols-md-5">
-			<div class="background-color-secondary text-color-white">hello</div>
-			<div class="background-color-secondary text-color-white">
-				<p class="text-align-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-			</div>
-			<div class="background-color-secondary text-color-white">obob</div>
-			<div class="background-color-secondary text-color-white">hello</div>
-			<div class="background-color-secondary text-color-white">bob</div>
-			<div class="background-color-secondary text-color-white">obob</div>
-			<div class="background-color-secondary text-color-white">hello</div>
-			<div class="background-color-secondary text-color-white">bob</div>
-			<div class="background-color-secondary text-color-white">obob</div>
-		</div>
-		@php echo wp_get_attachment_image(16, 'full') @endphp
 		@hasfield('block_heading')
 		<h2>@field('block_heading')</h2>
 		@endfield
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -6,14 +6,16 @@
 		<a href="/">@image($logo_color, 'full margin-0 logo')</a>
 	@endoption
   @if (has_nav_menu('primary_navigation'))
+	<div class="site-header__menu--rounded">
 		<nav id="nav-primary" class="nav-primary">
 			{!! wp_nav_menu(['theme_location' => 'primary_navigation', 'container' => false, 'menu_class' => 'menu list-reset']) !!}
-			<button id="menu-btn" class="hamburger hamburger--elastic" type="button"
-			aria-label="Menu" aria-controls="navigation" aria-expanded="true/false">
-				<span class="hamburger-box">
-					<span class="hamburger-inner"></span>
-				</span>
-			</button>
 		</nav>
+		<button id="menu-btn" class="hamburger hamburger--elastic" type="button"
+		aria-label="Menu" aria-controls="navigation" aria-expanded="true/false">
+			<span class="hamburger-box">
+				<span class="hamburger-inner"></span>
+			</span>
+		</button>
+	</div>
 	@endif
 </header>
